bd14a32b9d99888202097e1cfb78c66969a051e7
kvt337 <kayla015.kt@gmail.com>
2023-11-26 23:45:07 -0600

    added images folder

--- js/app.js
+++ js/app.js
@@ -1,31 +1,18 @@
-
-/*
- * Navigation Interactions
- * Toggle the mobile icon to show and hide the main navigation
- * Toggle the dropdown button to show and hide the dropdown content
-*/
 document.addEventListener('DOMContentLoaded', () => {
     const mobileToggler = document.querySelector('.navbar-toggler');
     const navigation = document.querySelector(mobileToggler.getAttribute('data-target'));
-
+  
     let dropdownToggle = document.querySelectorAll('.dropdown-toggle');
-    
+  
     mobileToggler.addEventListener('click', () => navigation.classList.toggle('collapse'))
-
+  
     dropdownToggle.forEach((dropdown) => {
-        dropdown.addEventListener('click', (toggler) => {
-            let dropdownElement = dropdown.parentElement;
-            dropdownElement.classList.toggle('collapse');
-        })
+      dropdown.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        let dropdownElement = dropdown.parentElement;
+        dropdownElement.classList.toggle('collapse');
+        dropdown.classList.toggle('active'); // Add this line
+      })
     })
-});
-
-
-/* 
-    * Ensure that the DOM is loaded before running
-    * the functions inside
-*/
-document.addEventListener('DOMContentLoaded', () => {
-    reportWindowSize();
-    window.onresize = reportWindowSize;
-});
+  });
+  

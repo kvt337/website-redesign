9f296f7926053847ca76398f32af82b8de8d2387
kvt337 <kayla015.kt@gmail.com>
2023-12-01 02:26:51 -0600

    finished donate and events

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const mobileToggler = document.querySelector('.navbar-toggler');
     const navigation = document.querySelector(mobileToggler.getAttribute('data-target'));
@@ -14,5 +15,49 @@ document.addEventListener('DOMContentLoaded', () => {
         dropdown.classList.toggle('active'); // Add this line
       })
     })
+    
+    /*
+    const more_btns = document.querySelectorAll('.more-btn.btn.btn-blue');
+    const less_btns = document.querySelectorAll('.less-btn.btn.btn-blue');
+    const event_infos = document.querySelectorAll('.event-limit, .event-spots, .event-desc, .btn.btn-blue');
+    const mediaQuery = window.matchMedia('(max-width: 766px)');
+    
+    if (mediaQuery.matches) {
+      more_btns.forEach((more_btn, index) => {
+        more_btn.addEventListener('click', () => {
+          event_infos.forEach(event_info => {
+            event_info.style.display = 'block';
+          });
+          more_btn.style.display = 'none';
+          less_btns[index].style.display = 'inline-block';
+        });
+      });
+    
+      less_btns.forEach((less_btn, index) => {
+        less_btn.addEventListener('click', () => {
+          event_infos.forEach(event_info => {
+            event_info.style.display = 'none';
+          });
+          more_btns[index].style.display = 'inline-block';
+          less_btn.style.display = 'none';
+        });
+      });
+    } else {
+      event_infos.forEach(event_info => {
+        event_info.style.display = 'block';
+      });
+    
+      more_btns.forEach(more_btn => {
+        more_btn.style.display = 'none';
+      });
+    
+      less_btns.forEach(less_btn => {
+        less_btn.style.display = 'none';
+      });
+    }
+    */
+    
+    
+
   });
   
